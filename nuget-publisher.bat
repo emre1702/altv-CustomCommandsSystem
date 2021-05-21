64c4df601874f5cda2667468acc3c789948aa543
emre1702 <emre1702@live.de>
2021-05-21 02:30:54 +0200

    Remove debug output

--- nuget-publisher.bat
+++ nuget-publisher.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
 ECHO Publishing the newest nupkg file.
 FOR /F "delims=|" %%I IN ('DIR "Nupkg\*.nupkg" /B /O:D') DO SET NewestFile=%%I
-ECHO %NewestFile%
-ECHO %CUSTOMCOMMANDSYSTEM_NUGET_KEY%
 start ./nuget.exe push Nupkg\%NewestFile% %CUSTOMCOMMANDSYSTEM_NUGET_KEY% -Source https://api.nuget.org/v3/index.json
 PAUSE
