bd7051213732ce23023aeb7512f46991d957355b
emre1702 <emre1702@live.de>
2021-06-24 22:37:11 +0200

    Use the same event as Chat resource for messages

--- Integration/ClientEvents.cs
+++ Integration/ClientEvents.cs
@@ -1,6 +1,9 @@
 ﻿using CustomCommandsSystem.Services;
 using AltV.Net.Elements.Entities;
 using AltV.Net;
+using AltV.Net.Elements.Args;
+using System;
+using CustomCommandsSystem.Services.Utils;
 
 namespace CustomCommandsSystem.Integration
 {
@@ -8,10 +11,25 @@ namespace CustomCommandsSystem.Integration
     {
         public ClientEvents()
         {
-            Alt.OnClient<IPlayer, string>("CustomCommandsSystem:Call", ExecuteCommand);
+            Alt.OnClient<IPlayer, string>("chat:message", OnChatMessage, OnChatMessageParser);
         }
 
-        private void ExecuteCommand(IPlayer player, string command)
-            => CommandsHandler.Instance?.ExecuteCommand(player, command);
+        private void OnChatMessage(IPlayer player, string message)
+        {
+            if (string.IsNullOrEmpty(message)) return;
+
+            var config = CommandsConfiguration.LazyInstance.Value;
+            if (!message.StartsWith(config.CommandPrefix)) return;
+
+            CommandsHandler.Instance?.ExecuteCommand(player, message);
+        }
+
+        private static void OnChatMessageParser(IPlayer player, MValueConst[] mValueArray, Action<IPlayer, string> action)
+        {
+            if (mValueArray.Length != 1) return;
+            var arg = mValueArray[0];
+            if (arg.type != MValueConst.Type.String) return;
+            action(player, arg.GetString());
+        }
     }
 }
