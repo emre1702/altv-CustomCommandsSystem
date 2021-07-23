f4514bb1e496e7e06a629d96e188250ecbc39432
emre1702 <emre1702@live.de>
2021-07-23 09:05:03 +0200

    Update nuget key environment variable name

--- nuget-publisher.bat
+++ nuget-publisher.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Publishing the newest nupkg file.
 FOR /F "delims=|" %%I IN ('DIR "Nupkg\*.nupkg" /B /O:D') DO SET NewestFile=%%I
-start ./nuget.exe push Nupkg\%NewestFile% %CustomCommandsSystem_NUGET_KEY% -Source https://api.nuget.org/v3/index.json
+start ./nuget.exe push Nupkg\%NewestFile% %CUSTOMCOMMANDSYSTEM_NUGET_KEY% -Source https://api.nuget.org/v3/index.json
 PAUSE
